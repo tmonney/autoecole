d11c3c9aeb541cc9ed1a90b383a1bb612c996449
Thierry Monney <thierry.monney@gmail.com>
2014-05-09 09:24:31 +0200

    Handle detached heads in prompt

--- project/ShellPrompt.scala
+++ project/ShellPrompt.scala
@@ -20,7 +20,27 @@ object ShellPrompt {
   val magenta = "\033[0;35m"
   val cyan = "\033[0;36m"
 
-  def branchDirty = {
+  sealed trait Ref
+  case class LocalBranch(local: String) extends Ref
+  case class TrackedBranch(local: String, upstream: String) extends Ref
+  case class DetachedHead(sha1: String) extends Ref
+
+  def isGitRepo = {
+    ("git rev-parse --git-dir" ! devnull) == 0
+  }
+
+  def currentRef = {
+    ("git rev-parse --symbolic-full-name HEAD" !!).trim match {
+      case "HEAD" => DetachedHead(("git rev-parse --short HEAD" !!).stripLineEnd)
+      case ref => Seq("refname", "upstream") map (refLabel(ref)) match {
+        case Seq(Some(branch), Some(upstream)) => TrackedBranch(branch, upstream)
+        case Seq(Some(branch), None) => LocalBranch(branch)
+        case _ => sys.error("Unexpected git state")
+      }
+    }
+  }
+
+  def workingDirDirty = {
     ("git diff --quiet" ! devnull) != 0
   }
 
@@ -32,23 +52,28 @@ object ShellPrompt {
     s"${color}${text}${reset}"
   }
 
-  def headLabel(label: String) = {
-    val ref: String = "git symbolic-ref -q HEAD" !!
+  def refLabel(ref: String)(label: String): Option[String] = {
     val cmd: String = s"git for-each-ref --format=%(${label}:short) ${ref}"
-    Option((cmd !!).stripLineEnd) filter (_.trim.nonEmpty) getOrElse "<none>"
+    Option((cmd !!).trim) filter (_.nonEmpty)
+  }
+
+  def colorizeBranch(branch: String) =
+    colorize(branch, if (workingDirDirty) red else if (indexDirty) yellow else green)
+
+  def branchInfo = currentRef match {
+    case DetachedHead(sha1) => s"""${colorize("DETACHED at ", magenta)}${colorizeBranch(sha1)}"""
+    case LocalBranch(branch) => colorizeBranch(branch)
+    case TrackedBranch(branch, upstream) => s"${colorizeBranch(branch)}->${colorize(upstream, cyan)}"
   }
 
   val buildShellPrompt = {
     (state: State) =>
       {
-        val sbt = colorize("sbt:", yellow)
+        val sbt = colorize("sbt:", magenta)
         val projectId = Project.extract(state).currentProject.id
         val version = BuildSettings.buildVersion
         val project = colorize(s"${projectId}-${version}", blue)
-        val branch = colorize(headLabel("refname"), if (branchDirty) red else if (indexDirty) yellow else green)
-        val upstream = colorize(headLabel("upstream"), cyan)
-
-        s"${sbt}${project} [${branch}->${upstream}]: "
+        s"${sbt}${project} [${branchInfo}]> "
       }
   }
 }
